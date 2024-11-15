f3af42e58dbcf50634d4954d4aaaee5a0c436ba5
SalvaRG08 <1916778@alu.murciaeduca.es>
2024-11-15 17:21:54 +0100

    Elimino archivos duplicados

--- soccerRun.js
+++ soccerRun.js
@@ -1,204 +1,169 @@
-window.onload = function() { 
+window.onload = function () {
 
-	const TOPEDERECHA = 473;
-	const TOPEABAJO   = 470;
-	
-	let x=100;     
-	let y=350;   
-	let canvas;  
-	let ctx;     
-	let id;      
-	
-	let xIzquierda, xDerecha, yUp, yDown;
-	
-	let inicial = 0;
-	let posicion=0;                                        
-	let cambio=0;                                          
-		
-	let miFutbolista;
-	let imagen;
-	
-	/* let audioTope; */
-	
-	function Futbolista (x_, y_) {
-	
-	  this.x = x_;
-	  this.y = y_;
-		this.animacionFutbolista = [
-			[0, 210], [54, 210],   // Derecha
-			[0, 90], [51, 90], // Izquierda
-			[0, 195], [32, 195], // Arriba
-			[0, 260], [32, 260]  // Abajo
-		];
-	  this.velocidad = 1.4;
-	  this.tamañoX   = 23;
-	  this.tamañoY   = 40;	  
-	
-	}
-	
-	/* function reproducirAudio() {
+    const TOPEIZQUIERDA = 0;
+    const TOPEDERECHA = 577;
+    const TOPEABAJO = 360;
 
-		audioTope.currentTime = 0;
-		audioTope.play();
-	
-	}	*/
-	
-	Futbolista.prototype.generaPosicionDerecha = function() {
+    const NUMEROOBSTACULOS = 5;
+    const TAMAÑOOBSTACULOX = 25;
+    const TAMAÑOOBSTACULOY = 25;
 
-		this.x = this.x + this.velocidad;
-		
-		if (this.x > TOPEDERECHA) {
-			
-			this.x = TOPEDERECHA;   
-			/* reproducirAudio(); */	
-		}		
+    let x = 100;
+    let y = 350;
+    let canvas;
+    let ctx;
 
-	}
-	
+    let xIzquierda, xDerecha, yUp, yDown;
 
-	Futbolista.prototype.generaPosicionIzquierda = function() {
-		
-		this.x = this.x - this.velocidad;
+    let posicion = 0;
+    let inicial = 0;
 
-		if (this.x < 0) {
-			
-			this.x = 0;	   
-			/* reproducirAudio();	*/
-		}
+    let miFutbolista;
+    let imagen;
 
-	}
-	
-	Futbolista.prototype.generaPosicionArriba = function() {
-		
-		this.y = this.y - this.velocidad;
+    let todosLosObstaculos = [];
 
-		if (this.y < 0) {
-			
-			this.y = 0;	
-			/* reproducirAudio();	*/
-		}
-	}	
-	
-	
-	Futbolista.prototype.generaPosicionAbajo = function() {
-		
-		this.y = this.y + this.velocidad;
+	// ---------------------- Objetos ---------------------- //
 
-		if (this.y > TOPEABAJO) {
-			
-			this.y = TOPEABAJO;
-			/* reproducirAudio(); */	
-		}
-	}	
-	
-	
-	function pintaFutbolista() {
-		
-		ctx.clearRect(0, 0, 500, 500);
-		
-		if (xDerecha) {
-			
-			miFutbolista.generaPosicionDerecha();	   
-		}
-		
-		if (xIzquierda)  {
-			
-			miFutbolista.generaPosicionIzquierda();
-		}	  
-		
-		if (yUp)  {
-			
-			miFutbolista.generaPosicionArriba();
-		}
-		
-		if (yDown)  {
-			
-			miFutbolista.generaPosicionAbajo();
-		}
-					  
-		ctx.drawImage(miFutbolista.imagen,
-					  miFutbolista.animacionFutbolista[posicion][0],   
-					  miFutbolista.animacionFutbolista[posicion][1],	
-					  miFutbolista.tamañoX, 		 
-					  miFutbolista.tamañoY,	     
-					  miFutbolista.x,     
-					  miFutbolista.y,	 
-					  miFutbolista.tamañoX,		 
-					  miFutbolista.tamañoY);     
-	}
-	
-	function alternarAnimacionMovimiento() {
-		
-		if (xDerecha)   inicial = 0;
-		if (xIzquierda) inicial = 2;
-		/* if (yUp) inicial = 4;
-		if (yDown) inicial = 6; */
-		
-		posicion = inicial + (posicion + 1) % 2;
-	}
-	
-	function activaMovimiento(evt) {
+    function Futbolista(x_, y_) {
+        this.x = x_;
+        this.y = y_;
+        this.animacionFutbolista = [
+            [0, 210], [54, 210],
+            [0, 90], [51, 90],
+            [0, 195], [32, 195],
+            [0, 260], [32, 260],
+        ];
+        this.velocidad = 1.4;
+        this.tamañoX = 23;
+        this.tamañoY = 40;
+    }
 
-        switch (evt.keyCode) {
-		
-			case 37: 
-			  xIzquierda = true;
-			  break;
+    function Obstaculos(x_2, y_2, velocidad_2) {
+        this.x = x_2;
+        this.y = y_2;
+        this.velocidad = velocidad_2;
+        this.haAcabado = false;
+    }
 
-			case 39:
-			  xDerecha = true;
-			  break;
-		 	
-			case 38:
-			  yUp = true;
-			  break;
+	// ---------------------- Codigo futbolista ---------------------- //
 
-			case 40:
-			  yDown = true;
-			  break;		 
-		}
-	}
+    Futbolista.prototype.generaPosicionDerecha = function () {
+        this.x = Math.min(this.x + this.velocidad, TOPEDERECHA);
+    };
 
-	function desactivaMovimiento(evt){
+    Futbolista.prototype.generaPosicionIzquierda = function () {
+        this.x = Math.max(this.x - this.velocidad, TOPEIZQUIERDA);
+    };
 
-        switch (evt.keyCode) {
+    Futbolista.prototype.generaPosicionArriba = function () {
+        this.y = Math.max(this.y - this.velocidad, TOPEIZQUIERDA);
+    };
 
-			case 37: 
-			  xIzquierda = false;
-			  break;
+    Futbolista.prototype.generaPosicionAbajo = function () {
+        this.y = Math.min(this.y + this.velocidad, TOPEABAJO);
+    };
 
-			case 39:
-			  xDerecha = false;
-			  break;
+    function pintaFutbolista() {
 
-			case 38:
-			  yUp = false;
-			  break;
+        if (xDerecha) miFutbolista.generaPosicionDerecha();
+        if (xIzquierda) miFutbolista.generaPosicionIzquierda();
+        if (yUp) miFutbolista.generaPosicionArriba();
+        if (yDown) miFutbolista.generaPosicionAbajo();
 
-			case 40:
-			  yDown = false;
-			  break;        		  
+        ctx.drawImage(
+            miFutbolista.imagen,
+            miFutbolista.animacionFutbolista[posicion][0],
+            miFutbolista.animacionFutbolista[posicion][1],
+            miFutbolista.tamañoX,
+            miFutbolista.tamañoY,
+            miFutbolista.x,
+            miFutbolista.y,
+            miFutbolista.tamañoX,
+            miFutbolista.tamañoY
+        );
+    }
+
+    function alternarAnimacionMovimiento() {
+        if (xDerecha) inicial = 0;
+        if (xIzquierda) inicial = 2;
+        posicion = inicial + (posicion + 1) % 2;
+    }
+
+	// ---------------------- Codigo obstaculos ---------------------- //
+
+    Obstaculos.prototype.pintarObstaculos = function () {
+        ctx.fillRect(this.x, this.y, TAMAÑOOBSTACULOX, TAMAÑOOBSTACULOY);
+    };
+
+    Obstaculos.prototype.moverObstaculo = function () {
+        this.x = this.x - this.velocidad;
+        if (this.x <= TOPEIZQUIERDA) this.haAcabado = true;
+    };
+
+    function generaDatosObstaculos() {
+        for (let i = 0; i < NUMEROOBSTACULOS; i++) {
+            let y = Math.random() * TOPEABAJO;
+            let velocidad = 0.5 + Math.random() * 2;
+            let miObstaculo = new Obstaculos(TOPEDERECHA, y, velocidad);
+            todosLosObstaculos.push(miObstaculo);
         }
-	}	
-	
-	document.addEventListener("keydown", activaMovimiento, false);
-	document.addEventListener("keyup", desactivaMovimiento, false);	
+    }
 
-	canvas = document.getElementById("miCanvas");
+	// ---------------------- Animación general de objetos ---------------------- //
 
-	/* audioTope = document.getElementById("tope"); */
+    function animar() {
+        ctx.clearRect(0, 0, 600, 400);
 
-	ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#FF0000";
+        todosLosObstaculos.forEach((obstaculo) => {
+            if (!obstaculo.haAcabado) {
+                obstaculo.pintarObstaculos();
+                obstaculo.moverObstaculo();
+            }
+        });
 
-	imagen = new Image();
-	imagen.src="assets/sprite/Characters.png";
-	Futbolista.prototype.imagen = imagen;
+        pintaFutbolista();
+    }
 
-	miFutbolista = new Futbolista( x, y);		
+	// ---------------------- Captura de teclas ---------------------- //
 
-	id= setInterval(pintaFutbolista, 1000/50);	
+    document.addEventListener("keydown", activaMovimiento, false);
+    document.addEventListener("keyup", desactivaMovimiento, false);
 
-	id = setInterval(alternarAnimacionMovimiento, 1000/5);
+    function activaMovimiento(evt) {
+        switch (evt.keyCode) {
+            case 37: xIzquierda = true; break;
+            case 39: xDerecha = true; break;
+            case 38: yUp = true; break;
+            case 40: yDown = true; break;
+        }
+    }
+
+    function desactivaMovimiento(evt) {
+        switch (evt.keyCode) {
+            case 37: xIzquierda = false; break;
+            case 39: xDerecha = false; break;
+            case 38: yUp = false; break;
+            case 40: yDown = false; break;
+        }
+    }
+
+    canvas = document.getElementById("miCanvas");
+    ctx = canvas.getContext("2d");
+
+    imagen = new Image();
+    imagen.src = "assets/sprite/Characters.png";
+    Futbolista.prototype.imagen = imagen;
+
+    miFutbolista = new Futbolista(x, y);
+
+    generaDatosObstaculos();
+
+    setInterval(animar, 1000 / 30);
+
+    setInterval(alternarAnimacionMovimiento, 1000 / 5);
+};
 
 
-}
+  
